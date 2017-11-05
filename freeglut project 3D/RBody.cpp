85654a87f0696fa1c57bf9bd113a7825dc9732a1
AlvaroErrepeBass <alvrod09@ucm.es>
2017-11-05 23:02:25 +0100

    metido tensor de inercia, falta hacer objetos complejos

--- freeglut project 3D/RBody.cpp
+++ freeglut project 3D/RBody.cpp
@@ -1,7 +1,7 @@
 #include "RBody.h"
 
 
-RBody::RBody() : time(0), pasos(10)
+RBody::RBody(float masa, float tam) : time(0), pasos(10)
 {
 	std::vector<float>aux(3);
 	for (size_t i = 0; i < 3; i++)
@@ -51,16 +51,49 @@ void RBody::CalculaPos(float deltaTime){
 	pos.sumar(k1);	pos.sumar(k2); pos.sumar(k3); pos.sumar(k4);
 }
 
+void RBody::CalculaTensorInercia(float deltaTime){
+	float   Ixx, Iyy, Izz, Ixy, Ixz, Iyz;
+	Ixx = 0;  Iyy = 0;  Izz = 0;
+	Ixy = 0;  Ixz = 0;  Iyz = 0;
+	//for (i = 0; i<_NUMELEMENTS; i++)
+	//{
+	Ixx += 1 + masa * correctedPos.getY() * correctedPos.getY() + correctedPos.getZ() *correctedPos.getZ();
+	Iyy += 1 + masa * correctedPos.getZ() * correctedPos.getZ() + correctedPos.getX() *correctedPos.getX();
+	Izz += 1 + masa * correctedPos.getX() * correctedPos.getX() + correctedPos.getY() *correctedPos.getY();
+	Ixy += masa * correctedPos.getX() *correctedPos.getY();
+	Ixz += masa * correctedPos.getX() *correctedPos.getZ();
+	Iyz += masa * correctedPos.getY() *correctedPos.getX();
+	//}
+	// e11 stands for element on row 1 column 1, e12 for row 1 column 2, etc.
+	tensorInercia[0][0] = Ixx;
+	tensorInercia[0][1] = -Ixy;
+	tensorInercia[0][2] = -Ixz;
+	tensorInercia[1][0] = -Ixy;
+	tensorInercia[1][1] = Iyy;
+	tensorInercia[1][2] = -Iyz;
+	tensorInercia[2][0] = -Ixz;
+	tensorInercia[2][1] = -Iyz;
+	tensorInercia[2][2] = Izz;
+}
+
 void RBody::update(float deltaTime, PuntoVector3D fuerzas = PuntoVector3D(0.0f, 9.81f, 0.0f,0)){
 	time += deltaTime;
 	delete sumFuerzas;
 	sumFuerzas = fuerzas.clonar();
 
+	fuerzas.mult(deltaTime);
+	vel.sumar(&fuerzas);
+
 	CalculaPos(deltaTime);
-	centroGravedad = pos.clonar();
+	centroGravedad = pos.clonar(); // pendiente de meter objetos dependientes dentro del rigid body para darle formas complejas
+	correctedPos = *(pos.clonar());
 
+	PuntoVector3D* aux = centroGravedad->clonar();
+	aux->escalar(-1);
+	correctedPos.sumar(aux);
+	delete aux;
 
-
+	CalculaTensorInercia(deltaTime);
 }
 
 PuntoVector3D* RBody::calculak(PuntoVector3D* x, PuntoVector3D* v, float deltaTime){
