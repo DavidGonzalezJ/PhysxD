9c52a79028197de0c0b60b2fe2bf9f3b48c18b5c
AlvaroErrepeBass <alvrod09@ucm.es>
2017-11-05 23:04:15 +0100

    Cambios en rigidBody

--- freeglut project 3D/RBody.h
+++ freeglut project 3D/RBody.h
@@ -7,7 +7,7 @@ class RBody :
 	public Objeto3D
 {
 public:
-	RBody();
+	RBody(float masa, float tam);
 	virtual ~RBody();
 	void update(float deltaTime, PuntoVector3D fuerzas = PuntoVector3D(0.0f, 9.81f, 0.0f, 0));
 private:
@@ -19,9 +19,11 @@ private:
 	PuntoVector3D* centroGravedad;
 	PuntoVector3D* sumFuerzas;
 	Quaternion* orientation;
+	PuntoVector3D correctedPos;
 	std::vector<std::vector<float>> tensorInercia;
 	float masa;
 	PuntoVector3D* calculak(PuntoVector3D* x, PuntoVector3D* v, float deltaTime);
 	void CalculaPos(float deltaTime);
+	void CalculaTensorInercia(float deltaTime);
 };
 
